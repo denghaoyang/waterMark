5a047dbfe95418d02abf3ddfef49c63f88c0bb29
denghy <275111108@qq.com>
2019-02-22 16:51:02 +0800

    commit

---
+++ application/index/controller/Base.php
@@ -0,0 +1,31 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+
+class Base extends Controller
+{
+    public function _initialize()
+    {
+        $this->view->engine->layout(true);
+    }
+
+    public function changeNullTo(&$data) {
+        if (is_array($data)) {
+            foreach ($data as &$val) {
+                if (is_array($val) || is_object($val)) {
+                    $val = is_object($val)?$val->toArray():$val;
+                    $this->changeNullTo($val);
+                } else {
+                    if (is_null($val)) {
+                        $val = "";
+                    }
+                }
+            }
+        } else {
+            if($data == null) {
+                $data = "";
+            }
+        }
+        return $data;
+    }
+}
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,65 +1,25 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use app\index\controller\Base;
+
 use app\index\model\IndexModel;
 
-class Index extends Controller
+
+class Index extends Base
 {
     public function index(){
-        $this->view->engine->layout(true);
         return $this->fetch();
     }
 
-    public function getRecordList()
-    {
-        $indexModel = new IndexModel();
-        $list = $indexModel->getListByFileId(2);
-        return json_encode($this->changeNullTo($list));
+    //水印嵌入记录
+    public function inList(){
+        $this->view->engine->layout(true);
+        return  $this->fetch("inList");
     }
 
-    public function saveRecordList($GUID,
-                                   $FileGUID,
-                                   $EmbedTime,
-                                   $SourceNodeGUID,
-                                   $DestNodeGUID,
-                                   $UserGUID,
-                                   $WatermarkIndex,
-                                   $WatermarkContent,
-                                   $Remark)
-    {
-        $data = [];
-        $data['guid'] = $GUID;
-        $data['fileGuid'] = $FileGUID;
-        $data['embedTime'] = $EmbedTime;
-        $data['sourceNodeGuid'] = $SourceNodeGUID;
-        $data['destNodeGuid'] = $DestNodeGUID;
-        $data['userGuid'] = $UserGUID;
-        $data['watermarkIndex'] = $WatermarkIndex;
-        $data['watermarkContent'] = $WatermarkContent;
-        $data['remark'] = $Remark;
-
-        $indexModel = new IndexModel();
-        $list = $indexModel->saveList($data);
-        return json_encode($this->changeNullTo($list));
-    }
-
-    public function changeNullTo(&$data) {
-        if (is_array($data)) {
-            foreach ($data as &$val) {
-                if (is_array($val) || is_object($val)) {
-                    $val = is_object($val)?$val->toArray():$val;
-                    $this->changeNullTo($val);
-                } else {
-                    if (is_null($val)) {
-                        $val = "";
-                    }
-                }
-            }
-        } else {
-            if($data == null) {
-                $data = "";
-            }
-        }
-        return $data;
+    //水印导出记录
+    public function outList(){
+        return  $this->fetch("outList");
     }
 }
